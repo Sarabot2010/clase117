1265f169fd1addc2f0d021f7ca2683ce06ee0b09
Sarabot2010 <99358625+Sarabot2010@users.noreply.github.com>
2022-10-29 12:47:25 -0500

    Add files via upload

---
+++ Clase 117/main.js
@@ -0,0 +1,37 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas() {
+    background("white");
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+    
+    }
+    function draw(){
+    strokeWeight (8);
+    stroke(0);
+    if (mouseIsPressed){
+    line (pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+      function classifyCanvas(){
+    
+    classifier.classify(canvas, gotResult);
+      }
+    function gotResult(error, results){
+    
+    if(error){
+    
+    console.log(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML-"Etiqueta:"+results[0].label;
+    document.getElementById("confidence").innerHTML-"Precisión:"+Math.round(results[0].confidence*100)+"%";
+}
